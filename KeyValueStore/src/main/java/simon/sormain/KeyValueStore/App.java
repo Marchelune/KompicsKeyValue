339c9e1467f48e4ab3db64d644fda69792fee61a
Rémi Sormain <remi.sormain@gmail.com>
2016-02-08 12:56:21 +0100

    pom created and set for kompics, for now.

---
+++ KeyValueStore/src/main/java/simon/sormain/KeyValueStore/App.java
@@ -0,0 +1,13 @@
+package simon.sormain.KeyValueStore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
