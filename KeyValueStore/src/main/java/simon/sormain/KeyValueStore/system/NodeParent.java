dbdc38854dd1df733417933475417f78789a4fb0
Rémi Sormain <remi.sormain@gmail.com>
2016-02-27 21:24:21 +0100

    Connected Multipaxos. Init almost done, need to retrieve the node rank from the conf file.

--- KeyValueStore/src/main/java/simon/sormain/KeyValueStore/system/NodeParent.java
+++ KeyValueStore/src/main/java/simon/sormain/KeyValueStore/system/NodeParent.java
@@ -11,11 +11,14 @@ import se.sics.kompics.network.netty.NettyNetwork;
 import se.sics.kompics.timer.Timer;
 import se.sics.kompics.timer.java.JavaTimer;
 import simon.sormain.KeyValueStore.epfd.Epfd;
+import simon.sormain.KeyValueStore.network.SetTAddress;
 import simon.sormain.KeyValueStore.network.TAddress;
 import simon.sormain.KeyValueStore.rBroadcast.BEBroadcastComponent;
 import simon.sormain.KeyValueStore.rBroadcast.BEBroadcastPort;
 import simon.sormain.KeyValueStore.rBroadcast.RegularReliableBroadcast;
-
+import simon.sormain.KeyValueStore.asc.AbortableSequenceConsensusPort;
+import simon.sormain.KeyValueStore.asc.MultiPaxos;
+import simon.sormain.KeyValueStore.asc.MultiPaxosInit;
 
 
 
@@ -25,15 +28,20 @@ public class NodeParent extends ComponentDefinition {
     Positive<Timer> timer = requires(Timer.class);
     
 	public NodeParent() {
+		TAddress selfAddress = config().getValue("keyvaluestore.self", TAddress.class);
+		SetTAddress alladdr = config().getValue("keyvaluestore.epfd.allAddr", SetTAddress.class);//still think this is weird :p
+		
 		//create and connect all components except timer and network
-        Component epfd = create(Epfd.class, Init.NONE); //TODO
-        Component beb = create(BEBroadcastComponent.class, Init.NONE); //TODO
-        //Component rb = create(RegularReliableBroadcast.class, Init.NONE); //TODO
+        Component epfd = create(Epfd.class, Init.NONE); 
+        Component beb = create(BEBroadcastComponent.class, Init.NONE);
+        Component asc = create(MultiPaxos.class, new MultiPaxosInit(selfAddress, 0, alladdr.get())); //TODO rank
+
 
       //connect required internal components to network and timer
         connect(epfd.getNegative(Timer.class), timer, Channel.TWO_WAY);
         connect(epfd.getNegative(Network.class), network, Channel.TWO_WAY);
         connect(beb.getNegative(Network.class), network, Channel.TWO_WAY);
+        connect(asc.getNegative(Network.class), network, Channel.TWO_WAY);
         //connect(rb.getNegative(BEBroadcastPort.class), beb.getPositive(BEBroadcastPort.class), Channel.TWO_WAY);
         
 	}
