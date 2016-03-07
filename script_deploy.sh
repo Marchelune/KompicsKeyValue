c7d0f28ceec2056090104f20aa12a87949152bca
Charles-Elie SIMON <charles-elie.simon@ensimag.grenoble-inp.fr>
2016-03-07 15:18:52 +0100

    modif script ranges

--- script_deploy.sh
+++ script_deploy.sh
@@ -24,7 +24,7 @@ port=$(($i + 45670))
 echo "keyvaluestore.self.addr = \"$ip:$port\"" >> reference.conf
 echo "keyvaluestore.self.rank = $i" >> reference.conf
 echo "keyvaluestore.self.ranks = \"1;$ip:45671!2;$ip:45672!3;$ip:45673!4;$ip:45674!5;$ip:45675!6;$ip:45676\"" >> reference.conf
-echo "keyvaluestore.self.ranges = \"0:999;$ip:45671,$ip:45672,$ip:45673!1000:1999;$ip:45674,$ip:45675,$ip:45676\"" >> reference.conf
+echo "keyvaluestore.self.ranges = \"0:1000;$ip:45671,$ip:45672,$ip:45673!1000:2000;$ip:45674,$ip:45675,$ip:45676\"" >> reference.conf
 echo "keyvaluestore.epfd.initDelay = 3000" >> reference.conf
 echo "keyvaluestore.epfd.deltaDelay = 800" >> reference.conf
 cd ../../
